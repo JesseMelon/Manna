e719e52e5244841cfdd89d2ee4bed32c1c0280e2
JesseMelon <jessechambers2000@icloud.com>
2025-03-15 11:44:49 -0400

    optional clean in build

--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+# Flag to determine if we should clean
+CLEAN=false
+
+# Check for -c argument
+while getopts "c" opt; do
+    case $opt in
+        c) CLEAN=true;;
+        ?) echo "Usage: $0 [-c]"; exit 1;;
+    esac
+done
+
 echo "Generating build script"
-make clean
+
+# Only run make clean if -c was provided
+if [ "$CLEAN" = true ]; then
+    make clean
+fi
+
 ./thirdparty/bin/premake5/linux/premake5 gmake --os=linux
 make
